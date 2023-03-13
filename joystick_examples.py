026709676b99313b5a944a32df8733fbeb666993
Cyryl Sochacki <cyrylsochacki@gmail.com>
2023-03-14 00:33:12 +0100

    Create joystick_examples.py

---
+++ joystick_examples.py
@@ -0,0 +1,28 @@
+from microbit import *
+import joystick
+while True:
+    if JoyStick.Down.read_digital() == 0: 
+        print("A")
+        sleep(50)
+    elif JoyStick.Right.read_digital() == 0: 
+        print("B")
+        sleep(50)
+    elif JoyStick.Left.read_digital() == 0: 
+        print("C")
+        sleep(50)
+    elif JoyStick.Up.read_digital() == 0: 
+        print("D")
+        sleep(50)  
+    elif JoyStick.JX.read_analog() <400: # wychylenie osi X w lewo
+        print('Lewo')
+        sleep(50)
+    elif JoyStick.JX.read_analog() >700: # wychylenie osi X w prawo
+        print('Prawo')
+        sleep(50)
+    elif JoyStick.JY.read_analog() >700: # wychylenie osi X w prawo
+        print('Gora')
+        sleep(50)
+    elif JoyStick.JY.read_analog() <400: # wychylenie osi X w lewo
+        print('Dol')
+        sleep(50)
+        
